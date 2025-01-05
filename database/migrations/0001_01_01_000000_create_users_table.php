2a0e5f05f4888ff5cadcc8d7f94904f888987b90
frankpulido <frankpulido@me.com>
2025-01-05 18:45:44 +0100

    migration prepared to work with roles - forced to continue with sanctum, my xampp php prebuilt extension pack complicated installing laravel passport

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'player'])->default('player');  // Roles for Sanctum auth (I cannot install Passport)
             $table->rememberToken();
             $table->timestamps();
 
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -15,8 +16,12 @@ class UserSeeder extends Seeder
     {
         $user = new User;
         $user->name = 'Frank Pulido';
+        $user->nickname = 'frankpulido';
         $user->email = 'frankpulido@me.com';
+        $user->email_verified_at = now();
         $user->password = bcrypt('1234');
+        $user->role = 'admin';  // Role for Sanctum (I cannot install Passport in my XAMPP php version)
+        $user->remember_token = Str::random(10);
         $user->save();
         
         User::factory(12)->create();

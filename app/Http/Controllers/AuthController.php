bcbb279e714ae0411d45e299dd6e614873bd335f
frankpulido <frankpulido@me.com>
2025-01-07 11:09:26 +0100

    rubén gave go ahead for sanctum today - prepared environment to use sanctum and added handler with new method for unauthenticated users requests

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register (Request $request) {
+
+        $request->headers->set('Accept', 'application/json');
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'nickname' => [
+                'nullable',
+                'string',
+                'max:255',
+                function ($attribute, $value, $fail) {
+                    if ($value !== 'anonymous' && User::where('nickname', $value)->exists()) {
+                        $fail('The nickname must be unique unless it is "anonymous".');
+                    }
+                },
+            ],
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8|confirmed' // "confirm" means that 'password' == 'password_confirmation' in $request
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'nickname' => $validated['nickname'] ?? 'anonymous',
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        $token = $user->createToken($request->name);
+
+        return response()->json([
+            'message' => 'Player created successfully',
+            'player' => $user,
+            'your_token' => $token->plainTextToken
+        ], 201);
+    }
+
+    public function login (Request $request) {
+
+        $request->headers->set('Accept', 'application/json');
+        $request->validate([
+            'email' => 'required|email|exists:users',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            // user with $request->email non-existent or wrong $request->password invalidates login
+            return response()->json([
+                'message' => 'Unauthorized log in. Check email and password',
+            ], 401);
+        }
+        $token = $user->createToken($user->name);
+
+        return response()->json([
+            'message' => 'User successfully logged in.',
+            'player' => $user,
+            'your_token' => $token->plainTextToken
+        ], 201);
+    }
+
+    public function logout (Request $request) {
+
+        $request->headers->set('Accept', 'application/json');
+        $request->user()->tokens()->delete();
+        return response()->json([
+            'message' => 'User successfully logged out.'
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,29 +1,39 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlayerController;
 use App\Http\Controllers\PlayController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 /*
-I will create 2 Controllers : one of them for admin exclusive functions and the other for user access
+I had the idea of creating 2 Controllers : one of them for admin exclusive functions and the other for user access
 This has to be revised, I am not sure about it...
-PlayerController
-AdminController
+For the moment we have one user-specific and other for statistics
 */ 
 
+// Route::apiResource('plays', PlayController::class); // Study this Resource : creates ALL routes based on controller methods
+
 Route::post('/players', [PlayerController::class, 'store']); // Create a player
-Route::put('/players/{id}', [PlayerController::class, 'update']); // Update player name
-Route::post('/players/{id}/games', [PlayController::class, 'store']); // Player rolls the dice and store outcome in db
-Route::delete('/players/{id}/games', [PlayController::class, 'destroy']); // Delete player's plays
 Route::get('/players', [PlayerController::class, 'index']); // List all players with the average success rate of each one of them
-Route::get('/players/{id}/games', [PlayController::class, 'index']); // List all plays for a given player
 Route::get('/players/ranking', [PlayerController::class, 'ranking']); // Average success across all plays (of all players)
 Route::get('/players/ranking/loser', [PlayerController::class, 'loser']); // Returns player with lowest success rate
 Route::get('/players/ranking/winner', [PlayerController::class, 'winner']); // Returns player with highest success rate
+
+Route::middleware('auth:sanctum')->group(function () {
+Route::put('/players/{id}', [PlayController::class, 'update']); // Update player name
+Route::post('/players/{id}/games', [PlayController::class, 'play']); // Player rolls the dice and store outcome in db
+Route::delete('/players/{id}/games', [PlayController::class, 'destroy']); // Delete player's plays
+Route::get('/players/{id}/games', [PlayController::class, 'history']); // List all plays for a given player
+});
+
+Route::post('/register', [AuthController::class, 'register']); // Retiurns confirmation message for user's registration
+Route::post('/login', [AuthController::class, 'login']); // Retiurns confirmation message for user's log in
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum'); // Deletes user's tokens and returns confirmation message for user's log out
 
 ?>
